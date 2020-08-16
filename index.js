9653e26177464577d433d044298e419d3579496c
Brito Alex <66937645+britoalex212@users.noreply.github.com>
2020-08-16 13:55:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// Routes
-module.exports = (app, config, bucket, partials, _) => {
-  require('./home')(app, config, bucket, partials, _)
-  require('./blog')(app, config, bucket, partials, _)
-  require('./contact')(app, config, bucket, partials, _)
-  require('./search')(app, config, bucket, partials, _)
-  require('./faqs')(app, config, bucket, partials, _)
-  require('./page')(app, config, bucket, partials, _)
-}
+// config
+const config = require('./' + process.env.NODE_ENV)
+module.exports = config
